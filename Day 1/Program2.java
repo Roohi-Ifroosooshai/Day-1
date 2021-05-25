7869790895a56c5dafa2fd6f04383ead696dad8c
Roohi-Ifroosooshai <34651878+Roohi-Ifroosooshai@users.noreply.github.com>
2021-05-25 17:38:12 +0530

    Update Program2.java

--- Day 1/Program2.java
+++ Day 1/Program2.java
@@ -1,65 +1,72 @@
+
+
 import java.util.*;
 
-class Rectangle
-{
-  static String input;
-  Rectangle(String input)
-  {
-	this.input=input;
-   }
-   static void display()
-   {
-	System.out.println("This is "+input);
-  }
+ interface Shape {
+   void display();
+}
 
+class Rectangle implements Shape {
+
+   public void display() {
+      System.out.println("This is Rectangle.");
+   }
+}
+
+class Square implements Shape {
+
+   public void display() {
+      System.out.println("This is Square.");
+   }
+}
+
+class Circle implements Shape {
+
+   public void display() {
+      System.out.println("This is Circle.");
+   }
+}
+
+class ShapeFactory {
+    
+   public Shape getShape(String shape){
+       
+      if(shape == null){
+         return null;
+      }		
+      if(shape.equalsIgnoreCase("Circle")){
+         return new Circle();
+         
+      } else if(shape.equalsIgnoreCase("Rectangle")){
+         return new Rectangle();
+         
+      } else if(shape.equalsIgnoreCase("Square")){
+         return new Square();
+      }
+      return null;
+   }
 }
 
 
-class Circle
-{
-  static String input;
-  Circle(String input)
-  {
-	this.input=input;
-   }
-   static void display()
-   {
-	System.out.println("This is "+input);
-  }
+public class Program2 {
 
-}
-
-class Square
-{
-  static String input;
-  Square(String input)
-  {
-	this.input=input;
-   }
-   static void display()
-   {
-	System.out.println("This is "+input);
-  }
-
-}
-
-public class Program2
-{
-	public static void main(String[] args) {
-	    System.out.println("Ener input shape: "); 
+   public static void main(String[] args) {
+       
+      ShapeFactory shapeFactory = new ShapeFactory();
+      
+      System.out.println("Ener input shape: "); 
 		Scanner sr=new Scanner(System.in);
 		String input=sr.next();
-		Rectangle r = new Rectangle(input);
-		Circle c = new Circle(input);
-		Square s = new Square(input);
-		
-		if (input.toLowerCase() == "rectangle")
-		    Rectangle.display();
-		else if (input.toLowerCase() == "circle")
-		    Circle.display();
-		else 
-		    Square.display();
-	}
+      
+      Shape shape = shapeFactory.getShape(input);
+      
+        if(shape.equals("CIRCLE"))
+            shape.display();
+        else if(shape.equals("RECTANGLE"))
+            shape.display();
+        else
+            shape.display();
+   }
 }
 
 
