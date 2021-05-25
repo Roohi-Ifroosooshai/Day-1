b4632f3d4cc50db03610577edbf33fec98435318
Roohi-Ifroosooshai <34651878+Roohi-Ifroosooshai@users.noreply.github.com>
2021-05-25 17:40:32 +0530

    Update Program4.java

--- Day 1/Program4.java
+++ Day 1/Program4.java
@@ -1,68 +1,97 @@
 
-
-
 import java.util.*;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.TreeSet;
-
-class Employee { 
-  //@Required  
-  private String firstName;
-  private String lastName;
-  private int age;  
-  private String company;
-  private int experience;
-
-  public Employee(String firstName, String lastName, int age, String company, int experience) {         
-    this.firstName = firstName;    
-    this.lastName = lastName;
-    this.age = age;
-    this.company = company;
-    this.experience = experience;
-  }       
-
-  public String getFirstName() {         
-    return firstName;     
-  }      
-  
-  public String getLastName() {         
-    return lastName;     
-  }       
-
-  public int getAge() {         
-    return age;     
-  }       
-  
-  public String getCompany() {         
-    return company;     
-  } 
-  
-  public int getExperience() {         
-    return experience;     
-  } 
-  
-  public String toString() {         
-    return " FirstName: " + this.firstName + ", LastName:" + this.lastName + ", Age:" + this.age + ", Company: " + this.company + ", Experience:" + this.experience ;     
-  } 
+class Employee
+{
+    private final String firstName;
+    private final String lastName;
+    private final int age;
+    private final String company;
+    private final int experience;
+ 
+    private Employee(EmployeeBuilder e) {
+        this.firstName = e.firstName;
+        this.lastName = e.lastName;
+        this.age = e.age;
+        this.company = e.company;
+        this.experience = e.experience;
+    }
+    
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public int getAge() {
+        return age;
+    }
+    public String getCompany() {
+        return company;
+    }
+    public int getExperience() {
+        return experience;
+    }
+ 
+    public String toString() {
+        return this.firstName+", "+this.lastName+", "+this.age+", "+this.company+", "+this.experience;
+    }
+ 
+    static class EmployeeBuilder 
+    {
+        private final String firstName;
+        private String lastName;
+        private int age;
+        private String company;
+        private int experience;
+ 
+        public EmployeeBuilder(String firstName) {
+            this.firstName = firstName;
+        }
+        public EmployeeBuilder lastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+        public EmployeeBuilder age(int age) {
+            this.age = age;
+            return this;
+        }
+        public EmployeeBuilder company(String company) {
+            this.company = company;
+            return this;
+        }
+        public EmployeeBuilder experience(int experience) {
+            this.experience = experience;
+            return this;
+        }
+        
+        public Employee build() {
+            Employee employee =  new Employee(this);
+            return employee;
+        }
+    }
 }
 
-
-public class Program4 {
-    public static void main(String[] args) throws Exception
-    {
-        Employee e1 = new Employee("John", "smith", 26, "IBM", 8);         
-        Employee e2 = new Employee("Bob", "Mary", 34, "Wipro", 2);  
+public class Program4{
+    public static void main(String[] args) {
+        
+        Employee e1 = new Employee.EmployeeBuilder("Lokesh")
+        .age(30)
+        .build();
         
         Set<Employee> employee1= new HashSet<Employee>();
         employee1.addAll(Arrays.asList(e1));
         
+        System.out.println("Employee1: " +employee1);
+     
+        Employee e2 = new Employee.EmployeeBuilder("Jack")
+        .company("Wipro")
+        .experience(6)
+        .build();
+        
         Set<Employee> employee2= new HashSet<Employee>();
         employee2.addAll(Arrays.asList(e2));
      
-    	System.out.println("Employee 1: " +employee1);
-    	System.out.println("Employee 2: " +employee2);
-    
+        System.out.println("Employee2: " +employee2);
     }
 }
